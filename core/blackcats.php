8be07bbd00df349cdb5e94b11ef70e625f42eccc
skartstudio <skartstudio21@gmail.com>
2023-08-25 00:17:32 -0500

    librerias

--- core/blackcats.php
+++ core/blackcats.php
@@ -5,7 +5,7 @@ require_once BACKCATS_THEME_DIR . '/core/login.php';
 require_once BACKCATS_THEME_DIR . '/core/menus.php';
 
 function blackcats_styles (){
-
+    wp_enqueue_style('maya-grip', get_stylesheet_directory_uri(). '/assets/librerias/mayagrip/mayagrip-min.css' );
 }
 add_action('wp_enqueue_scripts', 'blackcats_styles');
 
