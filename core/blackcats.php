3f494ea6fc217d8da52961ca222344aa75274983
skartstudio <skartstudio21@gmail.com>
2023-08-25 01:39:58 -0500

    Libreria animaciones

--- core/blackcats.php
+++ core/blackcats.php
@@ -7,6 +7,7 @@ require_once BACKCATS_THEME_DIR . '/core/menus.php';
 function blackcats_styles (){
     wp_enqueue_style('maya-grip', get_stylesheet_directory_uri(). '/assets/librerias/mayagrip/mayagrip-min.css' );
     wp_enqueue_style('fontawesome', get_stylesheet_directory_uri(). '/assets/librerias/fontawesome-free-6.4.2-web/css/all.min.css' );
+    wp_enqueue_style('maya-imagenover', get_stylesheet_directory_uri(). '/assets/librerias/imagenover/imagehover.min.css' );
 }
 add_action('get_footer', 'blackcats_styles');
 
