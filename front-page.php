b39a5bfcce86d8a021d2775ba63c041cd162211d
skartstudio <skartstudio21@gmail.com>
2023-08-25 00:03:18 -0500

    Frontpage y page

---
+++ front-page.php
@@ -0,0 +1,7 @@
+<?php get_header(); /* Template Name: Front-page */?>
+
+<?php get_template_part('template-parts/content'); ?>
+
+
+<?php get_footer(); ?>
+<!-- Front-page -->
---
+++ page.php
@@ -0,0 +1,7 @@
+<?php get_header(); /* Template Name: Page */?>
+
+<?php get_template_part('template-parts/content'); ?>
+
+
+<?php get_footer(); ?>
+<!-- Plantilla Page -->
