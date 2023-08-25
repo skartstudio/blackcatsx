142a6dd0fa0d6db5824e71ebb4960285b6b01fb8
skartstudio <skartstudio21@gmail.com>
2023-08-25 00:24:03 -0500

    anidando librerias

--- core/blackcats.php
+++ core/blackcats.php
@@ -6,6 +6,12 @@ require_once BACKCATS_THEME_DIR . '/core/menus.php';
 
 function blackcats_styles (){
     wp_enqueue_style('maya-grip', get_stylesheet_directory_uri(). '/assets/librerias/mayagrip/mayagrip-min.css' );
+    wp_enqueue_style('fontawesome', get_stylesheet_directory_uri(). '/assets/librerias/fontawesome-free-6.4.2-web/css/all.min.css' );
 }
-add_action('wp_enqueue_scripts', 'blackcats_styles');
+add_action('get_footer', 'blackcats_styles');
+
+function blackcats_styles_2 (){
+
+}
+add_action('wp_enqueue_scripts', 'blackcats_styles_2');
 
