e216d998e9f8719016fc8d9ddf0687f601358275
skartstudio <skartstudio21@gmail.com>
2023-08-27 00:45:11 -0500

    Footer

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
         <footer>
             <!-- Aquí colocar información del pie de página -->
+            <div class="mi-contenido-personalizado">
+                <p>© 2023 Mi Sitio Web. Todos los derechos reservados.</p>
+                <a href="#">Política de privacidad</a>
+            </div>
         </footer>
 
         <?php wp_footer(); ?>
