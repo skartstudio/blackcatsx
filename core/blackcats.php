ae82c70933041d242eac10d99bc91a7211e9468c
skartstudio <skartstudio21@gmail.com>
2023-09-09 07:26:53 -0500

    rework

--- core/blackcats.php
+++ core/blackcats.php
@@ -5,6 +5,7 @@ require_once BACKCATS_THEME_DIR . '/core/login.php';
 require_once BACKCATS_THEME_DIR . '/core/menus.php';
 
 function blackcats_styles (){
+    wp_enqueue_style('maya-reset', get_stylesheet_directory_uri(). '/assets/librerias/mayagrip/maya-reset.css' );
     wp_enqueue_style('maya-grip', get_stylesheet_directory_uri(). '/assets/librerias/mayagrip/mayagrip-min.css' );
     wp_enqueue_style('fontawesome', get_stylesheet_directory_uri(). '/assets/librerias/fontawesome-free-6.4.2-web/css/all.min.css' );
     wp_enqueue_style('maya-imagenover', get_stylesheet_directory_uri(). '/assets/librerias/imagenover/imagehover.css' );
@@ -15,4 +16,3 @@ function blackcats_styles_2 (){
 
 }
 add_action('wp_enqueue_scripts', 'blackcats_styles_2');
-
